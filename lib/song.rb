06c3e05d39a99924d37c8c39d0f33690fa3131ac
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-06-24 00:51:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,6 +37,13 @@ class Song
     self.create_by_name(name) || self.find_by_name(name)
   end
 
+  def self.new_from_filename(name)
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song
+  end
+
   def self.alphabetical
     @@all.sort_by{|s| s.name}
   end
